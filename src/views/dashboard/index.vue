61d4aa9cd7a8549f5d387a439b27f2bbdff64a91
D20F <2442563148@qq.com>
2021-11-17 10:14:30 +0800

    初始化

---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="dashboard-container">
+    <div class="dashboard-text">name: {{ username }}</div>
+  </div>
+
+  <div>update element-plus to 1.2.+, add svg icon, icon-font is abandoned</div>
+  <i class="el-icon-add-location">abandoned</i>
+  <div>
+    <el-icon>
+      <Svg.Edit></Svg.Edit>
+    </el-icon>
+  </div>
+</template>
+
+<script setup lang="ts">
+import * as Svg from '@element-plus/icons'
+import { computed } from 'vue'
+import { useStore } from 'vuex'
+
+let store = useStore()
+const username = computed(() => {
+  return store.state.user.username
+})
+</script>
+
+<style lang="scss" scoped>
+.dashboard {
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+  }
+}
+</style>
